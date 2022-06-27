ab04b8af06606dace05b2734ad0dc95bad6a8a21
Her <101386844+runingHer@users.noreply.github.com>
2022-06-27 15:10:45 +0800

    Update check_info%test.sh

--- check_info%test.sh
+++ check_info%test.sh
@@ -9,6 +9,11 @@ pcie_info=$(lspci -vvvs ${device_no} | grep "LnkSta:")
 check_nvme_info() {
   for nvme in ${nvme_info}; do
     smartctl -i /dev/${nvme} >${path}/${nvme}.log
+    if [ $? = 0 ]; then
+    echo -e "\033[\e[1;32m get nvme info success.........................................................please verify the result! \033[0m"
+    else
+    echo -e "\033[31m get nvme info failed!.........................................................please check! \033[0m"
+    fi
     cat ${path}/${nvme}.log
   done
 }
@@ -16,32 +21,28 @@ check_nvme_info() {
 check_sata_info() {
   for sata in ${sata_info}; do
     smartctl -i /dev/${sata} >${path}/${sata}.log
+    if [ $? = 0 ]; then
+    echo -e "\033[\e[1;32m get sata info success.........................................................please verify the result! \033[0m"
+    else
+    echo -e "\033[31m get sata info failed!.........................................................please check! \033[0m"
+    fi
     cat ${path}/${sata}.log
   done
 }
 #获取nvme_pcie速率
 check_pcie_info() {
   echo ${pcie_info} > ${path}/${device_no}
+  if [ $? = 0 ]; then
+    echo -e "\033[\e[1;32m get pcie info success.........................................................please verify the result! \033[0m"
+  else
+    echo -e "\033[31m get pcie info failed!.........................................................please check! \033[0m"
+  fi
   cat ${path}/${device_no}
 }
 echo "请校验nvme硬盘信息："
 check_nvme_info
-if [ $? = 0 ]; then
-    echo -e "\033[\e[1;32m get nvme info success.........................................................please verify the result! \033[0m"
-else
-    echo -e "\033[31m get nvme info failed!.........................................................please check! \033[0m"
-fi
 echo "请校验sata硬盘信息："
 check_sata_info
-if [ $? = 0 ]; then
-    echo -e "\033[\e[1;32m get sata info success.........................................................please verify the result! \033[0m"
-else
-    echo -e "\033[31m get sata info failed!.........................................................please check! \033[0m"
-fi
 echo "请校验nvme_pcie速率信息："
 check_pcie_info
-if [ $? = 0 ]; then
-    echo -e "\033[\e[1;32m get pcie info success.........................................................please verify the result! \033[0m"
-else
-    echo -e "\033[31m get pcie info failed!.........................................................please check! \033[0m"
-fi
+

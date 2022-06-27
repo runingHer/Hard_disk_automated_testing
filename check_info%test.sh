52b929fd8fa5113c56f7943a23b4f523b3e987a3
Her <101386844+runingHer@users.noreply.github.com>
2022-06-27 15:23:46 +0800

    Update check_info%test.sh

--- check_info%test.sh
+++ check_info%test.sh
@@ -9,35 +9,35 @@ pcie_info=$(lspci -vvvs ${device_no} | grep "LnkSta:")
 check_nvme_info() {
   for nvme in ${nvme_info}; do
     smartctl -i /dev/${nvme} >${path}/${nvme}.log
+    cat ${path}/${nvme}.log
     if [ $? = 0 ]; then
     echo -e "\033[\e[1;32m get nvme info success.........................................................please verify the result! \033[0m"
     else
     echo -e "\033[31m get nvme info failed.........................................................please check! \033[0m"
     fi
-    cat ${path}/${nvme}.log
   done
 }
 #获取sata_smart信息
 check_sata_info() {
   for sata in ${sata_info}; do
     smartctl -i /dev/${sata} >${path}/${sata}.log
+    cat ${path}/${sata}.log
     if [ $? = 0 ]; then
     echo -e "\033[\e[1;32m get sata info success.........................................................please verify the result! \033[0m"
     else
     echo -e "\033[31m get sata info failed.........................................................please check! \033[0m"
     fi
-    cat ${path}/${sata}.log
   done
 }
 #获取nvme_pcie速率
 check_pcie_info() {
   echo ${pcie_info} > ${path}/${device_no}
+  cat ${path}/${device_no}
   if [ $? = 0 ]; then
     echo -e "\033[\e[1;32m get pcie info success.........................................................please verify the result! \033[0m"
   else
     echo -e "\033[31m get pcie info failed.........................................................please check! \033[0m"
   fi
-  cat ${path}/${device_no}
 }
 echo "请校验nvme硬盘信息："
 check_nvme_info

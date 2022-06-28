10e795f3286e4cd09faf95829f90b7411acbdee8
Her <101386844+runingHer@users.noreply.github.com>
2022-06-28 10:52:28 +0800

    Update environment_variable.sh

--- environment_variable.sh
+++ environment_variable.sh
@@ -4,7 +4,7 @@ export sata_info=$(lsscsi | grep "/dev/sd" | awk -F "/" '{print $NF}')
 install_fio() {
   yum install -y libaio-devel || apt install -y libaio-dev
   yum install -y nvme*
-  yum install -y fio || apt install fio
+  yum install -y fio || apt install -y fio
   yum install -y sysstat || apt install -y sysstat
-  yum install smartmontools || apt install smartmontools
+  yum install -y smartmontools || apt install -y smartmontools
 }
